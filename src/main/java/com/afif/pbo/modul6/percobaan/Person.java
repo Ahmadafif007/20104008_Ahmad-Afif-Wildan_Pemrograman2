37f3ce5bcbbe205236c269742db199024ba57eba
Ahmadafif007 <ahmadafif69516@gmail.com>
2021-11-16 08:39:55 +0700

    modul6

---
+++ src/main/java/com/afif/pbo/modul6/percobaan/Person.java
@@ -0,0 +1,33 @@
+package com.afif.pbo.modul6.percobaan;
+
+public class Person {
+    protected String name;
+    protected String address;
+
+    public Person(){
+        System.out.println("Inside Person : Constructor");
+        name = "";
+        address = "";
+    }
+
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
